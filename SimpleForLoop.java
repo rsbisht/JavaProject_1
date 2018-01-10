fea4abaf1dca4ad21749e141a4c9e1d30a6a7d98
Rajendra Singh Bisht <rsbisht@hpe.com>
2018-01-10 15:38:11 +0530

    Modified SimpleForLoop.java. No. of lines decreased to 5

--- SimpleForLoop.java
+++ SimpleForLoop.java
@@ -2,7 +2,7 @@ public class SimpleForLoop {
 
    public static void main(String[] args) {
 
-       for (int i = 1; i <= 10; i++) {
+       for (int i = 1; i <= 5; i++) {
 
            System.out.println("This is Line No.... " + i);
        }
